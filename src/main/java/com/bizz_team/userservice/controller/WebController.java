ec52e4e57266e4ea4f4f8f72b3af2e38ce70a2da
rahul <rahul@rahul-Gadget>
2019-01-10 01:11:17 +0530

    first commite

--- src/main/java/com/bizz_team/userservice/controller/WebController.java
+++ src/main/java/com/bizz_team/userservice/controller/WebController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class WebController {
 	@GetMapping(value="/")
     public String homepage(){
-        return "register";
+        return "index1";
     }
 }
